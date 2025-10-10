014ef273cfc053dc1e8ecf413c8ff51aa03802fd
martianwho <83482515+martianwho@users.noreply.github.com>
2025-10-10 23:30:35 +0100

    Tardigrade tummy grows

--- tartigrades/TardigradeGameController.gd
+++ tartigrades/TardigradeGameController.gd
@@ -1,31 +1,39 @@
 extends Node2D
 const TILESIZE = 16
-const MOVEDELAY = .15
+var moveDelay = .15
 var moveTimer = 0
 var direction = Vector2(1,0)
 var tardigradeBody = []
 
 func _ready() -> void: 
 	for i in range(3):
-		var part = createPart(Vector2(-i, 0), i==0 )
+		var part = createPart(Vector2(-i, 0), i )
 		tardigradeBody.append(part)
 	pass
 		
 func _process(delta: float) -> void:
 	moveTimer += delta
-	if moveTimer >= MOVEDELAY:
+	if moveTimer >= moveDelay:
 		moveTimer = 0
 		moveTardigrade()
 		
-	if Input.is_action_just_pressed("ui_up"):
-		direction = Vector2(0,-1)
-	if Input.is_action_just_pressed("ui_down"):
-		direction = Vector2(0,1)
-	if Input.is_action_just_pressed("ui_left"):
-		direction = Vector2(-1,0)
-	if Input.is_action_just_pressed("ui_right"):
-		direction = Vector2(1,0)
-	
+	if Input.is_action_just_pressed("ui_up") and direction != Vector2(0, 1):
+		direction = Vector2(0, -1)
+	if Input.is_action_just_pressed("ui_down") and direction != Vector2(0, -1):
+		direction = Vector2(0, 1)
+	if Input.is_action_just_pressed("ui_left") and direction != Vector2(1, 0):
+		direction = Vector2(-1, 0)
+	if Input.is_action_just_pressed("ui_right") and direction != Vector2(-1, 0):
+		direction = Vector2(1, 0)
+
+func growTardigrade():
+	var middle = tardigradeBody[1]
+	var sprite = middle.get_node_or_null("Sprite2D")
+	if sprite:
+		sprite.scale *= Vector2(1.2, 1.2) 
+		
+	moveDelay *= .95
+
 func moveTardigrade():
 	var nextPosition = tardigradeBody[0].position + direction * TILESIZE
 	
@@ -36,17 +44,30 @@ func moveTardigrade():
 	
 	pass	
 
-func createPart(position: Vector2, ishead: bool):
+func createPart(position: Vector2, index: int):
 	var part = CharacterBody2D.new()
 	part.position = position *  TILESIZE
 	
 	var sprite = Sprite2D.new()
-	sprite.texture = preload("res://icon.svg")
+	
+	match index: 
+		0: 
+			sprite.texture = preload("res://icon.svg")
+		1: 
+			sprite.texture = preload("res://middle.svg.png")
+		2: 
+			sprite.texture = preload("res://tail.svg")
+		_: 
+			sprite.texture = preload("res://middle.svg")
+			
+	sprite.name = "Sprite2D"
 	sprite.scale = Vector2(.2,.2)
 	part.add_child(sprite)
+	
 	var collisionShape = CollisionShape2D.new()
 	var shape = RectangleShape2D.new()
 	shape.extents = Vector2(TILESIZE/2, TILESIZE/2)
+	
 	collisionShape.shape = shape
 	part.add_child(collisionShape)
 	
---
+++ tartigrades/food.gd
@@ -0,0 +1,11 @@
+extends Area2D
+const TILESIZE = 16
+
+func _ready() -> void:
+	position = Vector2(randi_range(0,40) * TILESIZE, randi_range(0,30) * TILESIZE)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	body.get_parent().growTardigrade()
+	queue_free()
+	pass # Replace with function body.
