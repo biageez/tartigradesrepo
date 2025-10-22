7b0161fc84cc2938b204dc6a1f3837494ddb6a11
martianwho <83482515+martianwho@users.noreply.github.com>
2025-10-22 22:30:41 +0100

    player animation

--- tartigrades/player/Prototype_Player.gd
+++ tartigrades/player/Prototype_Player.gd
@@ -7,27 +7,34 @@ extends CharacterBody2D
 
 #var speed: int = base_speed
 
-@export var speed:float = 200
-
+@export var speed := 30
+@export var animation_tree: AnimationTree
+var input
+var playback : AnimationNodeStateMachinePlayback
 #@onready var currency_node = get_node("res://currency/Prototype_Save.gd")
+
+func _ready():
+	playback = animation_tree["parameters/playback"]
 
 func _physics_process(delta: float) -> void:
 	#var currency = currency_node.currency
-	
 	#speed = clamp(base_speed + (currency * currency_speed_multiplier), base_speed, max_speed)
-	
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
+	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input * speed
 	move_and_slide()
+	select_animation()
+	update_animation_parameters()
 	
-	if Input.is_action_pressed("ui_down"):
-		$AnimatedSprite2D.play("player_walk_down")
+func select_animation():
+	if velocity == Vector2.ZERO:
+		playback.travel("Idle")
+	else: 
+		playback.travel("Walk")
 	
-	if Input.is_action_pressed("ui_up"):
-		$AnimatedSprite2D.play("player_walk_up")
-		
-	if Input.is_action_pressed("ui_left"):
-		$AnimatedSprite2D.play("player_walk_left")
-		
-	if Input.is_action_pressed("ui_right"):
-		$AnimatedSprite2D.play("player_walk_right")
+func update_animation_parameters():
+	if input == Vector2.ZERO:
+		return
+	
+	animation_tree["parameters/Idle/blend_position"] = input
+	animation_tree["parameters/Walk/blend_position"] = input
+	
