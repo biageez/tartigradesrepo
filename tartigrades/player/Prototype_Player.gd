2fe21530c9355ee1d95871bf914ef8b2555ccf5f
martianwho <83482515+martianwho@users.noreply.github.com>
2025-10-27 23:59:21 +0000

    not finished fixing but good enough for now

--- tartigrades/player/Prototype_Player.gd
+++ tartigrades/player/Prototype_Player.gd
@@ -1,33 +1,45 @@
 class_name Player
 extends CharacterBody2D
 
+@export var speed : float = 200
+@export var animation_tree: AnimationTree
+
+var input : Vector2
+var playback : AnimationNodeStateMachinePlayback
+
+func _ready(): 
+	playback = animation_tree["parameters/playback"]
+
+func _physics_process(delta: float) -> void:
+	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input * speed
+	move_and_slide()
+	select_animation()
+	update_animation_parameters()
+
+func select_animation():
+	if velocity == Vector2.ZERO:
+		playback.travel("Idle")
+	else: 
+		playback.travel("Walk")
+
+func update_animation_parameters():
+	if input == Vector2.ZERO: 
+		return
+	animation_tree["parameters/Walk/blend_position"] = input
+	animation_tree["parameters/Idle/blend_position"] = input
+
+# ---------------Notes---------------
+
 #var base_speed: int = 15
 #var max_speed := 500
 #var currency_speed_multiplier := 0.5
 
 #var speed: int = base_speed
 
-@export var speed:float = 200
-
 #@onready var currency_node = get_node("res://currency/Prototype_Save.gd")
 
-func _physics_process(delta: float) -> void:
-	#var currency = currency_node.currency
+#var currency = currency_node.currency
 	
 	#speed = clamp(base_speed + (currency * currency_speed_multiplier), base_speed, max_speed)
 	
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
-	move_and_slide()
-	
-	if Input.is_action_pressed("ui_down"):
-		$AnimatedSprite2D.play("player_walk_down")
-	
-	if Input.is_action_pressed("ui_up"):
-		$AnimatedSprite2D.play("player_walk_up")
-		
-	if Input.is_action_pressed("ui_left"):
-		$AnimatedSprite2D.play("player_walk_left")
-		
-	if Input.is_action_pressed("ui_right"):
-		$AnimatedSprite2D.play("player_walk_right")
