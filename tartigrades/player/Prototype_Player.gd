94e84f5045980e374f3ebd84680a6a28beccb067
martianwho <83482515+martianwho@users.noreply.github.com>
2025-10-29 23:32:34 +0000

    trying to get belly to lag behind but couldn't figure it out

--- tartigrades/player/Prototype_Player.gd
+++ tartigrades/player/Prototype_Player.gd
@@ -9,6 +9,7 @@ var playback : AnimationNodeStateMachinePlayback
 
 func _ready(): 
 	playback = animation_tree["parameters/playback"]
+	spawn_belly()
 
 func _physics_process(delta: float) -> void:
 	input = Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
@@ -16,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	select_animation()
 	update_animation_parameters()
-
+	
 func select_animation():
 	if velocity == Vector2.ZERO:
 		playback.travel("Idle")
@@ -28,6 +29,31 @@ func update_animation_parameters():
 		return
 	animation_tree["parameters/Walk/blend_position"] = input
 	animation_tree["parameters/Idle/blend_position"] = input
+
+func spawn_belly(): 
+	var belly_scene = preload("res://player/Prototype_Player_Belly.tscn")
+	var sprite_node = get_node("Sprite2D")
+	
+	#--- First Belly ---
+	var belly1 = belly_scene.instantiate()
+	sprite_node.add_child(belly1)
+	belly1.position = Vector2(0,560.0)
+	belly1.set_meta("target_position", belly1.position)
+	var anim_belly1 = belly1.get_node_or_null("AnimationPlayer")
+	if anim_belly1: 
+		anim_belly1.play("Belly_Walk")
+
+	#--- Second Belly ---
+	var belly2 = belly_scene.instantiate()
+	sprite_node.add_child(belly2)
+	belly2.position = Vector2(0, 1080)
+	belly2.scale.x = -1
+	belly2.set_meta("target_position", belly2.position)
+	var anim_belly2 = belly2.get_node_or_null("AnimationPlayer")
+	if anim_belly2: 
+		anim_belly2.play("Belly_Walk")
+		
+
 
 # ---------------Notes---------------
 
