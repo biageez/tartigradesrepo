07f0b6803c58384fc784db19ad11a6acb6322aea
martianwho <83482515+martianwho@users.noreply.github.com>
2025-11-28 22:29:41 +0000

    attempts to have second body rotate/linger after head

--- tartigrades/player/Prototype_Player.gd
+++ tartigrades/player/Prototype_Player.gd
@@ -3,9 +3,15 @@ extends CharacterBody2D
 
 @export var speed : float = 200
 @export var animation_tree: AnimationTree
+@export var rotation_speed := 1.0
 
 var input : Vector2
 var playback : AnimationNodeStateMachinePlayback
+
+var sprite_head
+var belly1 
+var belly2
+
 
 func _ready(): 
 	playback = animation_tree["parameters/playback"]
@@ -14,9 +20,11 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	input = Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
 	velocity = input * speed
+	
 	move_and_slide()
 	select_animation()
 	update_animation_parameters()
+	rotate_belly(delta)
 	
 func select_animation():
 	if velocity == Vector2.ZERO:
@@ -32,27 +40,27 @@ func update_animation_parameters():
 
 func spawn_belly(): 
 	var belly_scene = preload("res://player/Prototype_Player_Belly.tscn")
-	var sprite_node = get_node("Sprite2D")
+	sprite_head = get_node("Sprite2D")
 	
 	#--- First Belly ---
-	var belly1 = belly_scene.instantiate()
-	sprite_node.add_child(belly1)
+	belly1 = belly_scene.instantiate()
+	sprite_head.add_child(belly1)
 	belly1.position = Vector2(0,560.0)
-	belly1.set_meta("target_position", belly1.position)
 	var anim_belly1 = belly1.get_node_or_null("AnimationPlayer")
 	if anim_belly1: 
 		anim_belly1.play("Belly_Walk")
 
 	#--- Second Belly ---
-	var belly2 = belly_scene.instantiate()
-	sprite_node.add_child(belly2)
+	belly2 = belly_scene.instantiate()
+	sprite_head.add_child(belly2)
 	belly2.position = Vector2(0, 1080)
-	belly2.scale.x = -1
-	belly2.set_meta("target_position", belly2.position)
+	#belly2.scale.x = -1
 	var anim_belly2 = belly2.get_node_or_null("AnimationPlayer")
 	if anim_belly2: 
 		anim_belly2.play("Belly_Walk")
 		
+func rotate_belly(delta):
+	belly1.rotation = lerp_angle(belly1.rotation, sprite_head.rotation, rotation_speed * delta)
 
 
 # ---------------Notes---------------
